1e76ba861e9f5b5c00cbe9f9896b19896472f023
JEREMY FRANCOIS <JEREM.FRANCOIS@gmail.com>
2023-08-12 09:57:55 +0200

    donnees fetchees triees, dedoublonnées

--- app/[components]/GetTopCitiesNquality.js
+++ app/[components]/GetTopCitiesNquality.js
@@ -1,44 +1,60 @@
-'use client'
+"use client";
 
-import {useState, useEffect} from 'react';
-import CardTopCitiesNquality from './CardTopCitiesNquality';
+import { useState, useEffect } from "react";
+import CardTopCitiesNquality from "./CardTopCitiesNquality";
 
 const GetTopCitiesNquality = () => {
+  const [data, setData] = useState([]);
 
-
-const [data, setData] = useState([]);
-
-useEffect (()=> {
-fetch('https://hubeau.eaufrance.fr/api/v1/qualite_eau_potable/resultats_dis?conformite_limites_bact_prelevement=N&conformite_limites_pc_prelevement=N')
-    .then((response) => response.json())
-    .then((res) => setData(res.data))
-    .catch(() => console.error(err))
-
-}, [])
-
-
-    return (
-        <>
-        <ul className='flex flex-wrap max-w-full w-3/4 mx-auto '>
-        {data
-        .map((el) => {
-            return(
-              
-                <CardTopCitiesNquality key={el.id} id={el.id} nom_commune={el.nom_commune} nom_departement={el.nom_departement} />
-
-            )
-
-        })
-
-           
-        }
-
-    </ul>
-        
-        </>
-        
-        
+  const construireData = (dataFetched) => {
+    console.log("construire data");
+    setData(
+      dataFetched
+        .map((d) => ({
+          codeCommune: d.code_commune,
+          nomCommune: d.nom_commune,
+          nomDepartement: d.nom_departement,
+        }))
+        .reduce(
+          (agregation, courant) =>
+            !agregation.find((el) => el.codeCommune === courant.codeCommune)
+              ? [...agregation, courant]
+              : agregation,
+          []
+        )
     );
+    console.log("construire data fin");
+  };
+
+  useEffect(() => {
+    fetch(
+      "https://hubeau.eaufrance.fr/api/v1/qualite_eau_potable/resultats_dis?conformite_limites_bact_prelevement=N&conformite_limites_pc_prelevement=N"
+    )
+      .then((response) => response.json())
+      .then((res) => construireData(res.data))
+      .catch(() => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    console.log("DATA", data);
+  }, [data]);
+
+  return (
+    <>
+      <ul className="flex flex-wrap max-w-full w-3/4 mx-auto ">
+        {data.map((el) => {
+          return (
+            <CardTopCitiesNquality
+              key={el.codeCommune}
+              id={el.codeCommune}
+              nom_commune={el.nomCommune}
+              nom_departement={el.nomDepartement}
+            />
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default GetTopCitiesNquality;
--- app/page.js
+++ app/page.js
@@ -1,38 +1,39 @@
-import Geoloc from "./[components]/Geoloc"
-import GetTopCitiesNquality from "./[components]/GetTopCitiesNquality"
+import GetTopCitiesNquality from "./[components]/GetTopCitiesNquality";
 
 export default function Home() {
-
   return (
     <main className="">
-     
-     <header className='relative'> 
+      <header className="relative">
+        <img
+          className="h-1/2"
+          src="fond-texture-eau.jpg"
+          alt="image d acceuil E qual"
+        ></img>
 
+        <div className="flex justify-start content-center absolute top-10 left-10">
+          <img
+            className="w-2/5 hover:scale-105 "
+            src="gif-eau.png"
+            alt="gif eau robinet geant"
+          />
+        </div>
 
-          <img className='h-1/2' src='fond-texture-eau.jpg' alt='image d acceuil E qual'></img>
+        <div className=" opacity-80 w-2/5 absolute top-10 right-10 bg-sky-400 rounded-lg shadow-md hover:bg-sky-300 text-white p-5">
+          <h1 className="text-center text-3xl">Hey l'eau !</h1>
+          <br />
+          <p className="text-2xl text-center ">
+            Bienvenue sur le site national analysant la qualité de l'eau de
+            votre ville 😊
+            <br />
+            Par un simple clic, retrouvez les résultats de la conformité des
+            prélèvements
+            <br />
+            sur les dernières années.
+          </p>
+        </div>
+      </header>
 
-
-          <div className='flex justify-start content-center absolute top-10 left-10'> 
-            <img className='w-2/5 hover:scale-105 ' src='gif-eau.png' alt='gif eau robinet geant'/>
-          </div>
-
-
-          <div className=" opacity-80 w-2/5 absolute top-10 right-10 bg-sky-400 rounded-lg shadow-md hover:bg-sky-300 text-white p-5"> 
-            <h1 className="text-center text-3xl">Hey l'eau !</h1>
-            <br/>
-            <p className="text-2xl text-center ">Bienvenue sur le site national analysant la qualité de l'eau 
-              de votre ville 😊
-              <br/>
-              Par un simple clic, retrouvez les résultats de la conformité des
-              prélèvements 
-              <br/>sur les dernières années.
-            </p>
-          </div>
-        </header>
-
-<Geoloc/>
-<GetTopCitiesNquality/>
-
+      <GetTopCitiesNquality />
     </main>
-  )
+  );
 }
